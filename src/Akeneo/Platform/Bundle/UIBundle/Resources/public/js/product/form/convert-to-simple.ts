a8d05fb7c6eb73094e9e4acf007b8415ca3ef158
Mathias Métayer <mathias.metayer@akeneo.com>
2020-10-01 10:35:02 +0200

    WIP: cleanup component

--- src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/convert-to-simple.ts
+++ src/Akeneo/Platform/Bundle/UIBundle/Resources/public/js/product/form/convert-to-simple.ts
@@ -3,96 +3,91 @@ import * as _ from 'underscore';
 
 import BaseView = require('pimui/js/view/base');
 const Dialog = require('pim/dialog');
-const FormBuilder = require('pim/form-builder');
 const __ = require('oro/translator');
 const SecurityContext = require('pim/security-context');
 const messenger = require('oro/messenger');
 const Routing = require('routing');
 const router = require('pim/router');
-const LoadingMask = require('oro/loading-mask');
 
 const template = require('pim/template/product/convert-to-simple');
 
 interface Config {
-    form: string;
+    url: string;
+    successMessage: string;
 }
 
 class ConvertToSimple extends BaseView {
-  private readonly template = _.template(template);
+    private readonly template = _.template(template);
 
-  private readonly config: Config;
+    private readonly config: Config;
 
-  constructor(options: {config: Config}) {
-    super({...options, ...{className: 'AknDropdown-menuLink', tagName: 'button'}});
+    constructor(options: { config: Config }) {
+        super({...options, ...{className: 'AknDropdown-menuLink', tagName: 'button'}});
 
-    this.config = {...this.config, ...options.config};
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public events(): EventsHash {
-    return {
-      'click': this.convert,
-    };
-  }
-
-  public render(): BaseView {
-    if (
-      SecurityContext.isGranted('pim_enrich_product_create') &&
-      'product' === this.getFormData().meta.model_type &&
-      // true === this.getFormData().meta.is_owner &&   TODO Check permissions
-      null !== this.getFormData().parent
-    ) {
-      this.$el.html(this.template({
-        label: 'convert TODO translate',
-      }));
+        this.config = {...this.config, ...options.config};
     }
 
-    return BaseView.prototype.render.apply(this, arguments);
-  }
+    /**
+     * {@inheritdoc}
+     */
+    public events(): EventsHash {
+        return {
+            'click': this.convert,
+        };
+    }
 
-  private convert() {
-    return Dialog.confirm(
-      'content',
-      'title',
-      this.doConvert.bind(this),
-      'subtitle',
-      'buttonClass',
-      'buttonText',
-    )
-  }
-
-  private doConvert() {
-    const loadingMask = new LoadingMask();
-    this.$el.empty()
-      .append(loadingMask.render().$el.show());
-
-    fetch(Routing.generate(this.config.url, {
-      id: this.getFormData().meta.id,
-    }), {
-      headers: {
-        'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest',
-        // ...params.header,
-      },
-      method: 'POST',
-    })
-      .then((response) => {
-        if (response.ok) {
-          messenger.notify('success', __(this.config.trans.success));
-        } else {
-          messenger.notify('error', 'error');
+    public render(): BaseView {
+        if (
+            SecurityContext.isGranted('pim_enrich_product_create') &&
+            'product' === this.getFormData().meta.model_type &&
+            // true === this.getFormData().meta.is_owner &&   TODO Check permissions
+            null !== this.getFormData().parent
+        ) {
+            this.$el.html(this.template({
+                label: 'convert TODO translate',
+            }));
         }
-      })
-      .catch((e) => {
-        messenger.notify('error', e);
-      })
-      .finally(() => {
-          loadingMask.hide().$el.remove();
-          router.redirectToRoute(Routing.generate(this.config.redirectUrl));
-      });
-  }
+
+        return BaseView.prototype.render.apply(this, arguments);
+    }
+
+    private convert() {
+        return Dialog.confirm(
+            'content',
+            'title',
+            () => {
+                router.showLoadingMask();
+
+                fetch(Routing.generate(this.config.url, {
+                    id: this.getFormData().meta.id,
+                }), {
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Requested-With': 'XMLHttpRequest',
+                        // ...params.header,
+                    },
+                    method: 'POST',
+                })
+                    .then((response) => {
+                        if (response.ok) {
+                            messenger.notify('success', __(this.config.successMessage));
+                        } else {
+                            messenger.notify('error', 'error TODO');
+                        }
+                    })
+                    .catch((e) => {
+                        console.log(e);
+                    })
+                    .finally(() => {
+                        router.hideLoadingMask();
+                        router.reloadPage();
+                    });
+            },
+            'subtitle',
+            'buttonClass',
+            'buttonText',
+        )
+    }
 }
 
 export = ConvertToSimple;
